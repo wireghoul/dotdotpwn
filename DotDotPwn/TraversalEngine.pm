7e0e05e285a866c4c63709327f290722e0d0d75b
wireghoul <wireghoul@users.noreply.github.com>
2016-11-09 12:59:48 +1100

    Another null byte variant added

--- DotDotPwn/TraversalEngine.pm
+++ DotDotPwn/TraversalEngine.pm
@@ -48,6 +48,7 @@ my @Extra_files = ("config.inc.php", "web.config");
 # Dots (..) representations to be combined in the Traversal Engine
 our @Dots = ("..",
              ".%00.",
+             "..%00",
              "..%01",
              ".?", "??", "?.",
              "%5C..",
